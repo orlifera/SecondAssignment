abe76c6bb68a82ebe1c8cb2d420e8611db0f1101
P3lle <nicolop3lle@gmail.com>
2023-05-16 12:20:43 +0200

    Configurazione processo build e close #2

---
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -0,0 +1,12 @@
+package it.unipd.mtss;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IntegerToRomanTest {
+    @Test
+    public void test1() {
+//        assertEquals("I", IntegerToRoman.convert(1));
+    }
+}
---
+++ roman-number/src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -0,0 +1,12 @@
+package it.unipd.mtss;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RomanPrinterTest {
+    @Test
+    public void test1() {
+//        assertEquals("I", RomanPrinter.convert(1));
+    }
+}

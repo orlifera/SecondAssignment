ea058b089697c3627393c14fda6c1b237f6a1f58
P3lle <nicolop3lle@gmail.com>
2023-05-17 13:29:20 +0200

    Fixato codice sorgente

--- roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
+++ roman-number/src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -1,11 +1,87 @@
+////////////////////////////////////////////////////////////////////
+// ORLANDO FERAZZANI 2058653
+// NICOLÒ PELLEGRINELLI 2034334
+////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class RomanPrinter {
-    public static String print(int num){
+    private static HashMap<Character, String[]> converter = null;
+    static {
+        HashMap<Character, String[]> conMap = new HashMap<Character, String[]>();
+        conMap.put('I', new String[] { " _____ ",
+                "|_   _|",
+                "  | |  ",
+                "  | |  ",
+                " _| |_ ",
+                "|_____|" });
+        conMap.put('V', new String[] { "__      __",
+                "\\ \\    / /",
+                " \\ \\  / / ",
+                "  \\ \\/ /  ",
+                "   \\  /   ",
+                "    \\/    " });
+        conMap.put('X', new String[] { "__   __",
+                "\\ \\ / /",
+                " \\ V / ",
+                "  > <  ",
+                " / . \\ ",
+                "/_/ \\_\\" });
+        conMap.put('L', new String[] { " _      ",
+                "| |     ",
+                "| |     ",
+                "| |     ",
+                "| |____ ",
+                "|______|" });
+        conMap.put('C', new String[] { "  _____ ",
+                " / ____|",
+                "| |     ",
+                "| |     ",
+                "| |____ ",
+                " \\_____|" });
+        conMap.put('D', new String[] { " _____  ",
+                "|  __ \\ ",
+                "| |  | |",
+                "| |  | |",
+                "| |__| |",
+                "|_____/ " });
+        conMap.put('M', new String[] { " __  __ ",
+                "|  \\/  |",
+                "| \\  / |",
+                "| |\\/| |",
+                "| |  | |",
+                "|_|  |_|" });
+        converter = conMap;
+    }
+
+    public static String print(int num)
+            throws NumberBelowZeroException, NumberOverFiveThousand,
+            NRCException {
         return printAsciiArt(IntegerToRoman.convert(num));
     }
-    private static String printAsciiArt(String romanNumber){
-        //TODO
-        return null;
+
+    private static String printAsciiArt(String romanNumber)
+            throws NRCException {
+        if (romanNumber.equals("")) {
+            return "";
+        }
+        for (int c = 0; c < romanNumber.length(); c++) {
+            char currCh = romanNumber.charAt(c);
+            if (!(Arrays.asList('I', 'V', 'X', 'L', 'C', 'D', 'M').contains(currCh))) {
+                throw new NRCException(currCh);
+            }
+        }
+        StringBuilder asciiArt = new StringBuilder();
+
+        for (int i = 0; i < 6; i++) {
+
+            for (int c = 0; c < romanNumber.length(); c++) {
+                asciiArt.append(converter.get(romanNumber.charAt(c))[i]);
+            }
+            asciiArt.append('\n');
+        }
+        return asciiArt.toString();
     }
 }
